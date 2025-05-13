dfb76e9cc5c3c8354cd3c5bbe896bc6172ee24d9
Ramon <ramondiego90@gmail.com>
2025-05-13 14:50:41 -0300

    Adiciona script build para Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Container } from '@mui/material';
 
 import Turmas from './components/Admin/Turmas';
@@ -15,18 +15,24 @@ function App() {
     <Router>
       <Container maxWidth="md" sx={{ py: 2 }}>
         <Routes>
+          {/* Redireciona a raiz para /aluno/social */}
+          <Route path="/" element={<Navigate to="/aluno/social" replace />} />
+
           {/* Rotas Admin */}
           <Route path="/admin/turmas" element={<Turmas />} />
           <Route path="/admin/alunos" element={<Alunos />} />
 
           {/* Rotas Aluno com Layout global */}
           <Route path="/aluno" element={<StudentLayout />}>
+            {/* Tornar /aluno também redirecionar para /aluno/social */}
+            <Route index element={<Navigate to="social" replace />} />
             <Route path="social" element={<Social />} />
             <Route path="exercicios" element={<Exercicios />} />
             <Route path="aulas" element={<Aulas />} />
-            {/* REDIRECIONA /aluno para /aluno/social */}
-            <Route index element={<Social />} />
           </Route>
+
+          {/* Qualquer outra rota não reconhecida pode voltar pra home */}
+          <Route path="*" element={<Navigate to="/aluno/social" replace />} />
         </Routes>
       </Container>
     </Router>
